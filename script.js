7398181bab1ab5ae7b8198cc1c6e6bef35791150
JULIA RIBEIRO DA SILVA <JULIA_4647@uirapuru.br>
2025-10-10 14:17:38 -0300

    teste

--- script.js
+++ script.js
@@ -29,5 +29,18 @@ document.querySelector('.busca').addEventListener('submit', async(event) => {
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg;
-
+    
+    document.querySelector('.resultado').style.display = 'block'; 
 }
+
+function showInfo(json){
+    showWarning("");
+    document.querySelector('.resultado').style.display = 'block';
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp}/<sup>°C</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>Km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`
+}
+
